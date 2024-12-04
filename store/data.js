759598432cd8e0729ccf78ae66a5f705fe0da595
Ravindu Eshan <127772297+EshanUGR@users.noreply.github.com>
2024-12-04 18:54:28 +0530

    completed express tutorial

---
+++ store/data.js
@@ -0,0 +1,46 @@
+export const products = [
+  {
+    id: 1,
+    name: "albany sofa",
+    image: "akialsuppliers.com",
+    price: 45.5,
+    desc: `I'ma very proud about me`,
+  },
+  {
+    id: 2,
+    name: "albany sofa",
+    image: "akialsuppliers.com",
+    price: 45.5,
+    desc: `I'ma very proud about me`,
+  },
+  {
+    id: 3,
+    name: "albany sofa",
+    image: "akialsuppliers.com",
+    price: 45.5,
+    desc: `I'ma very proud about me`,
+  },
+  {
+    id: 4,
+    name: "albany sofa",
+    image: "akialsuppliers.com",
+    price: 45.5,
+    desc: `I'ma very proud about me`,
+  },
+];
+
+export const pepole = [
+  { id: 1, name: "john" },
+  {
+    id: 2,
+    name: "peter",
+  },
+  {
+    id: 3,
+    name: "johan",
+  },
+  {
+    id: 4,
+    name: "saman",
+  },
+];
